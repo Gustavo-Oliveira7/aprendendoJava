857aac428fb6081cccf59a1fd7f21ed4163c5b25
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-09-18 14:57:35 -0300

    exercicios dia 02 - fundamento de java

--- tiposdeDados/src/smartHouseExercicio.java
+++ tiposdeDados/src/smartHouseExercicio.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class smartHouseExercicio {
     private boolean lampadaLigada;
+    private House house;
+    private Map<String,String> estadosBrasileiros;
 
     public smartHouseExercicio(){
         this.lampadaLigada = false;
+        this.house = null;
+        this.estadosBrasileiros = new HashMap<String, String>();
+        this.estadosBrasileiros.put("SP", "São Paulo");
+        this.estadosBrasileiros.put("RJ", "Rio de Janeiro");
     }
 
     public boolean isLampadaLigada(){
@@ -13,5 +22,22 @@ public class smartHouseExercicio {
     }
     public void desligarLampada(){
         this.lampadaLigada = false;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+    public String mostraEstado(String uf) {
+        return this.estadosBrasileiros.get(uf);
+    }
+
+    //internet connection
+    public boolean conectarNet(){
+        for (int i = 0; i < 3; i++) {
+            if (Math.random() < 0.5) {
+                return true;
+            }
+        }
+        return false;
     }
 }
